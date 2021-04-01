cefb92181dd6a30a7d877523d0c88fb6af2a96e0
powerar <arpower@bell.net>
2021-04-01 13:56:55 -0400

    open and close modal for profile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
-$("button").click(function() {
+//show modal on profile button click
+$("#submit-button").click(function() {
     debugger;
-    $('#profile-modal').show();
+    $("#profile-modal").show();
 });
+
+//dismiss main modal when closed
+$(".close-btn").click(function() {
+    debugger;
+    $("#profile-modal").hide();
+});
+
+// $("#submit-button").click(function(){
+    
+// })
