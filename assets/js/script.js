471b25f2f75ca5789afce63aca2f1796a278d32f
powerar <arpower@bell.net>
2021-04-03 11:35:13 -0400

    merged in my code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 //globally clal userProfile as an object with empty attributes
 var userProfile = {
-    Age: "", 
+    Age: "",
     Weight: "",
     Height: ""
 };
 
 //show modal on profile button click
-$("#profile-button").click(function() {
+$("#profile-button").click(function () {
     $("#profile-modal").show();
 });
 
 //dismiss main modal when closed
-$(".close-btn").click(function() {
+$(".close-btn").click(function () {
     $("#profile-modal").hide();
 });
 
 //send user stats to userProfile object
-$("#submit-button").click(function() {
+$("#submit-button").click(function () {
     var userAge = $("input[id='age'").val();
     var userWeight = $("input[id='weight'").val();
     var userHeight = $("input[id='height'").val();
@@ -27,8 +27,11 @@ $("#submit-button").click(function() {
 });
 
 //save the user's profile to localstorage
-var saveProfile = function(userProfile) {
+var saveProfile = function (userProfile) {
     localStorage.setItem("userProfile", JSON.stringify(userProfile));
 };
 
+<<<<<<< Updated upstream
 //
+=======
+>>>>>>> Stashed changes
