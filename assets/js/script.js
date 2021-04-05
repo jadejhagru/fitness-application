fa77f3e38440e89b4b4c8108c6beecf4a175466a
Andre M <andre.ji.moseley@gmail.com>
2021-04-05 17:50:32 -0400

    pushing exercises list call

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,139 +90,21 @@ var showExercises = function(category) {
           selectedExerciseId = data.results[i].id;
           $("#category").append(exerciseEl);
         }
+
         $(".exercise-btn").click(function () {
 
-          var dynamicExercise = function (selectedExerciseId,exerciseName,dayId) { fetch(
-            'https://wger.de/api/v2/exercise/?format=json&limit=1000'
-          )
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(response) {
-
-            exerciseName.replace(" ","-");
-            exerciseName.replace(",","");
-            exerciseName = exerciseName.toLowerCase();
-          
-            $(".exercise-name."+exerciseName+"").text(response.results[selectedExerciseId].name);
-            $(".exercise-graphic."+exerciseName+"").attr('alt',response.results[selectedExerciseId].name);
-            $(".exercise-description."+exerciseName+"").append(response.results[selectedExerciseId].description);
-            $(".exercise-group."+exerciseName+"").text('Muscle group: Abs');
-          });
-
-          //giphy fetch
-          //
-          // fetch(
-          //   'https://api.giphy.com/v1/gifs/OgJiGwuIlVvgs?api_key=pEDYeIUt9R8XnZUzlutQsGdmtpuWCJqf'
-          // )
-          //   .then(function(response) {
-          //     return response.json();
-          //   })
-          //   .then(function(response) {
-          
-          //     $(".exercise-graphic."+exerciseName+"").attr('src',response.data.images.downsized_large.url);
-          
-          // })
-          createExerciseCard(exerciseName,dayId);
-          };
-          
-          //Creates exercise card object - needs be to scoped to the exercise option selection loop
-          var createExerciseCard = function(exerciseId,dayId) //dayId is the section it goes to in the day column
-          {
-            
-            //actual card code starts here 
-            $("#"+dayId+".card-body").append("<div class=\"card-body newdiv\" id="+dayId+"\"></div>");
-            var exerciseCard = $("#"+dayId+".card-body").find(".newdiv"); 
-
-            exerciseCard.addClass("card exercise-card "+exerciseId+"").attr("id",exerciseId);
-            
-            //Card image/GIF
-            exerciseCard.append("<img>");
-            exerciseCard.find("img").addClass("card-img-top exercise-graphic "+exerciseId+" "+dayId+"");
-          
-            //Card name
-            exerciseCard.append("<div class=\"card-body\">");
-            exerciseCard.find(".card-body").append("<h5>");
-            exerciseCard.find(".card-body h5").addClass("card-title exercise-name "+exerciseId+" "+dayId+"");
-          
-            //Card description
-            exerciseCard.find(".card-body").append("<div class=\"card-text exercise-description "+exerciseId+" "+dayId+"\">");
-          
-            //Card muscle group
-            exerciseCard.append("<ul class=\"list-group list-group-flush\">");
-            exerciseCard.find(".list-group").append("<li class=\"list-group-item exercise-group "+exerciseId+" "+dayId+"\">");
-          
-            //Card rest tip
-            exerciseCard.find(".list-group").append("<li class=\"list-group-item helpful-tip "+exerciseId+" "+dayId+"\">")
-            exerciseCard.find(".list-group .helpful-tip").text("Remember to rest between every set for 40 secs to 1 min");
-          
-            //Creates set,rep count and rep time selection button---
-            var buttonName = ["set","rep-count","rep-group"];
-            for (var i = 0; i < 3; i++)
-            {
-              exerciseCard.append("<div class=\"btn-group\" id=\""+buttonName[i]+"-button\">");
-          
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group").append("<div class=\"btn-group\" role=\"group\" id=\""+buttonName[i]+"-button\">");
-          
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").append("<button id=\""+buttonName[i]+"-button\">");
-          
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").addClass("btn btn-secondary btn-sm dropdown-toggle");
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").attr("type","button");
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").attr("data-toggle","dropdown");
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").attr("aria-haspopup","true");
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").attr("aria-expanded","false");
-          
-              var buttonText; // button menu name
-          
-              if (i === 0)
-              {
-                buttonText="Sets";
-              }
-              else if (i === 1)
-              {
-                buttonText="Rep Count";
-              }
-              else if(i === 2)
-              { 
-                buttonText="Rep Time";
-              }
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").text(buttonText);
-              exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").append("<label class=selected-value></label>");
-          
-              //creates menu options
-                exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").append("<div id=\""+buttonName[i]+"-options\">")
-                exerciseCard.find("#"+buttonName[i]+"-options").addClass("dropdown-menu exercise-set-rep dropdown-menu-right").attr("x-placement","bottom-start");
-          
-                if (i === 0) //Note: &#35 is to show the # character in the HTML href attribute
-                {
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">3</a>");
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">4</a>");
-                }
-                else if (i === 1)
-                {
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">5-8</a>");
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">8-10</a>");
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">10-12</a>");
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">12-15</a>");
-                }
-                else if(i === 2)
-                { 
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">15</a>");
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">30</a>");
-                  exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">60</a>");
-                }
-          
-            }
-          }
-
-          dynamicExercise(selectedExerciseId,exerciseName,dayVariable);//add day id arguement variable
+          createExerciseButton();
 
           $("#category").empty();
         });
       })
     }
-  });
+  })
 };
+
+var createExerciseButton = function () {
+
+}
 
 //event listenser to assign set and rep button values
 $(".exercise-card").on("click","a",function()
@@ -233,20 +115,20 @@ $(".exercise-card").on("click","a",function()
   $(this).parent().parent().parent().find(".selected-value").text(text);
 });
 
-//ANDRE'S OLD STUFF - Keep this commented
+// //ANDRE'S OLD STUFF - Keep this commented
 
 //using this to check the api list of exercises in the console log
-// fetch(
-//   'https://wger.de/api/v2/exercise/?format=json&limit=1000'
-// )
-// .then(function(response) {
-//   return response.json();
-// }).then(function(response) {console.log(response);})
+fetch(
+  'https://wger.de/api/v2/exercise/?format=json&limit=1000'
+)
+.then(function(response) {
+  return response.json();
+}).then(function(response) {console.log(response);})
 
 // //Old
 // //Holds all exercises
 // var exerciseList = {
-//   arms: [bicepCurls,'hammerCurls','tricepDip','tricepExtension'],
+//   arms: ['bicepCurls','hammerCurls','tricepDip','tricepExtension'],
 //   shoulders: ['lateralRaises','shoulderPressDumbbells','lateralFrontRaises','shoulderShrug'],
   
 //   chest: ['benchPress','inclineDumbbellPress','pushups'],
@@ -256,10 +138,6 @@ $(".exercise-card").on("click","a",function()
 //   legs: ['romanianDeadlift','squats','dumbbellLungesStanding'],
 //   calves: ['calfRaises','legCurl','legExtenstion']
 // }
-
-// //Uncomment this to run the cards
-// //exerciseList.abs[0](1,"bicep-curls"); // this syntax ->calls abs crunches //the dayIdber is the section it goes to in the day column
-// //exerciseList.arms[0](2,"crunches");
 
 // //Abs-Crunches
 // var absCrunches = function (dayRowId,exerciseName) { fetch(
@@ -275,6 +153,44 @@ $(".exercise-card").on("click","a",function()
 //   $(".exercise-group."+exerciseName+"").text('Muscle group: Abs');
 // });
 
+// fetch(
+//   'https://api.giphy.com/v1/gifs/OgJiGwuIlVvgs?api_key=pEDYeIUt9R8XnZUzlutQsGdmtpuWCJqf'
+// )
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(response) {
+
+//     $(".exercise-graphic."+exerciseName+"").attr('src',response.data.images.downsized_large.url);
+
+// })
+// createExerciseCard("crunches",1);
+// };
+// //
+
+// exerciseList.arms[0](2,"crunches");
+// //
+
+// var dynamicExercise = function (selectedExerciseId,exerciseName,dayId) { fetch(
+//   'https://wger.de/api/v2/exercise/?format=json&limit=1000'
+// )
+// .then(function(response) {
+//   return response.json();
+// })
+// .then(function(response) {
+
+//   exerciseName.replace(" ","-");
+//   exerciseName.replace(",","");
+//   exerciseName = exerciseName.toLowerCase();
+
+//   $(".exercise-name."+exerciseName+"").text(response.results[selectedExerciseId].name);
+//   $(".exercise-graphic."+exerciseName+"").attr('alt',response.results[selectedExerciseId].name);
+//   $(".exercise-description."+exerciseName+"").append(response.results[selectedExerciseId].description);
+//   $(".exercise-group."+exerciseName+"").text('Muscle group: Abs');
+// });
+
+// //giphy fetch
+
 // // fetch(
 // //   'https://api.giphy.com/v1/gifs/OgJiGwuIlVvgs?api_key=pEDYeIUt9R8XnZUzlutQsGdmtpuWCJqf'
 // // )
@@ -285,39 +201,103 @@ $(".exercise-card").on("click","a",function()
 
 // //     $(".exercise-graphic."+exerciseName+"").attr('src',response.data.images.downsized_large.url);
 
-// // })
-// createExerciseCard("crunches",1);
-// };
-// //
-
-// //Bicep-Curls
-// var bicepCurls = function (dayRowId,exerciseName) { fetch(
-//   'https://wger.de/api/v2/exercise/?format=json&limit=1000'
-// )
-// .then(function(response) {
-//   return response.json();
 // })
-// .then(function(response) {
-//   $(".exercise-name."+exerciseName+"").text(response.results[45].name);
-//   $(".exercise-graphic."+exerciseName+"").attr('alt',response.results[45].name);
-//   $(".exercise-description."+exerciseName+"").append(response.results[45].description);
-//   $(".exercise-group."+exerciseName+"").text('Musclue group: Arms');
-// });
 
-// // fetch(
-// //   'https://api.giphy.com/v1/gifs/HMzadBUQG3y53pYmOK?api_key=pEDYeIUt9R8XnZUzlutQsGdmtpuWCJqf'
-// // )
-// //   .then(function(response) {
-// //     return response.json();
-// //   })
-// //   .then(function(response) {
-// //     $(".exercise-graphic."+exerciseName+"").attr('src',response.data.images.downsized_large.url);
-// // })
-// createExerciseCard("bicep-curls",2);
-// };
-// //
+// createExerciseCard(exerciseName,dayId);
+// )};
 
-//
+
+// dynamicExercise(selectedExerciseId,exerciseName,dayVariable);//add day id arguement variable
+
+
+//Creates exercise card object - needs be to scoped to the exercise option selection loop
+// var createExerciseCard = function(exerciseId,dayId) //dayId is the section it goes to in the day column
+// {
+  
+//   //actual card code starts here 
+//   $("#"+dayId+".card-body").append("<div class=\"card-body newdiv\" id="+dayId+"\"></div>");
+//   var exerciseCard = $("#"+dayId+".card-body").find(".newdiv"); 
+
+//   exerciseCard.addClass("card exercise-card "+exerciseId+"").attr("id",exerciseId);
+  
+//   //Card image/GIF
+//   exerciseCard.append("<img>");
+//   exerciseCard.find("img").addClass("card-img-top exercise-graphic "+exerciseId+" "+dayId+"");
+
+//   //Card name
+//   exerciseCard.append("<div class=\"card-body\">");
+//   exerciseCard.find(".card-body").append("<h5>");
+//   exerciseCard.find(".card-body h5").addClass("card-title exercise-name "+exerciseId+" "+dayId+"");
+
+//   //Card description
+//   exerciseCard.find(".card-body").append("<div class=\"card-text exercise-description "+exerciseId+" "+dayId+"\">");
+
+//   //Card muscle group
+//   exerciseCard.append("<ul class=\"list-group list-group-flush\">");
+//   exerciseCard.find(".list-group").append("<li class=\"list-group-item exercise-group "+exerciseId+" "+dayId+"\">");
+
+//   //Card rest tip
+//   exerciseCard.find(".list-group").append("<li class=\"list-group-item helpful-tip "+exerciseId+" "+dayId+"\">")
+//   exerciseCard.find(".list-group .helpful-tip").text("Remember to rest between every set for 40 secs to 1 min");
+
+//   //Creates set,rep count and rep time selection button---
+//   var buttonName = ["set","rep-count","rep-group"];
+//   for (var i = 0; i < 3; i++)
+//   {
+//     exerciseCard.append("<div class=\"btn-group\" id=\""+buttonName[i]+"-button\">");
+
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group").append("<div class=\"btn-group\" role=\"group\" id=\""+buttonName[i]+"-button\">");
+
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").append("<button id=\""+buttonName[i]+"-button\">");
+
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").addClass("btn btn-secondary btn-sm dropdown-toggle");
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").attr("type","button");
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").attr("data-toggle","dropdown");
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").attr("aria-haspopup","true");
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").attr("aria-expanded","false");
+
+//     var buttonText; // button menu name
+
+//     if (i === 0)
+//     {
+//       buttonText="Sets";
+//     }
+//     else if (i === 1)
+//     {
+//       buttonText="Rep Count";
+//     }
+//     else if(i === 2)
+//     { 
+//       buttonText="Rep Time";
+//     }
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").find("button").text(buttonText);
+//     exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").append("<label class=selected-value></label>");
+
+//     //creates menu options
+//       exerciseCard.find("#"+buttonName[i]+"-button.btn-group[role=group]").append("<div id=\""+buttonName[i]+"-options\">")
+//       exerciseCard.find("#"+buttonName[i]+"-options").addClass("dropdown-menu exercise-set-rep dropdown-menu-right").attr("x-placement","bottom-start");
+
+//       if (i === 0) //Note: &#35 is to show the # character in the HTML href attribute
+//       {
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">3</a>");
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">4</a>");
+//       }
+//       else if (i === 1)
+//       {
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">5-8</a>");
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">8-10</a>");
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">10-12</a>");
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">12-15</a>");
+//       }
+//       else if(i === 2)
+//       { 
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">15</a>");
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">30</a>");
+//         exerciseCard.find("#"+buttonName[i]+"-options").append("<a class=\"dropdown-item\" href=\"&#35\">60</a>");
+//       }
+
+//   }
+// }
 
 
 
