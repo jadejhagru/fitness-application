70059917833b7a90ffd9f943a82260d1a4f30751
powerar <arpower@bell.net>
2021-04-05 21:15:42 -0400

    fixed bmi calculation bug

--- assets/js/bmi.js
+++ assets/js/bmi.js
@@ -1,15 +1,20 @@
 var bmiFinal = "";
-var h = document.getElementById('h').value;
-var w = document.getElementById('w').value;
+var h = "";
+var w = "";
 
 function BMI() {
+    if (!h && !w) {
+        h =$('#h').val().trim();
+        w = $('#w').val().trim();
+    } else {
     var bmi = w / (h / 100 * h / 100);
     var bmio = (bmi.toFixed(2));
 
     document.getElementById("result").innerHTML = "Your BMI is " + bmio;
     bmiFinal = bmio;
     saveBmi(bmiFinal);
-}
+    }
+};
 
 var loadProfile = function () {
     debugger;
@@ -19,14 +24,10 @@ var loadProfile = function () {
         w = loadedProfile.Weight;
         document.getElementById("h").setAttribute("placeholder", loadedProfile.Height);
         document.getElementById("w").setAttribute("placeholder", loadedProfile.Weight);
-        console.log(h);
-        console.log(w);
-        
     }
     else {
         return;
     }
-    BMI();
 };
 
 var saveBmi = function (bmiFinal) {
